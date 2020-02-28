38c7772c1e3251398414f32b193ed52d04e33df9
moddyz <moddy3d@gmail.com>
2020-02-29 01:01:32 +1300

    Replaced installBoost.sh with python script.

---
+++ buildUtils.py
@@ -0,0 +1,54 @@
+"""
+Build utilities.
+
+Snippets taken from USD/build_scripts/build_usd.py
+"""
+
+import os
+import urllib2
+import subprocess
+import shlex
+import tarfile
+
+
+def PrintInfo(message):
+    print("[INFO] {}".format(message))
+
+
+def MakeDirectories(directoryPath):
+    if not os.path.isdir(directoryPath):
+        PrintInfo("Making directories: {!r}".format(directoryPath))
+        os.makedirs(directoryPath)
+
+
+def ChangeDirectory(directoryPath):
+    PrintInfo("Changing directory: {!r}".format(directoryPath))
+    os.chdir(directoryPath)
+
+
+def RunCommand(command):
+    PrintInfo("Running shell command {!r}".format(command))
+    process = subprocess.Popen(shlex.split(command))
+    process.wait()
+
+
+def DownloadURL(url, dstFilePath):
+    if os.path.exists(dstFilePath):
+        PrintInfo("{!r} already exists! Skipping download.".format(dstFilePath))
+        return False
+
+    PrintInfo("Downloading {} -> {}".format(url, dstFilePath))
+    command = "curl {progress} -L -o {filename} {url}".format(
+        progress="-#",
+        filename=dstFilePath,
+        url=url
+    )
+    RunCommand(command)
+
+    return True
+
+
+def Unpack(srcFile, dstPath):
+    PrintInfo("Unpacking {} -> {}".format(srcFile, dstPath))
+    archive = tarfile.open(srcFile)
+    archive.extractall(dstPath)
---
+++ installBoost.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+
+from buildUtils import (
+    PrintInfo,
+    DownloadURL,
+    MakeDirectories,
+    Unpack,
+    ChangeDirectory,
+    RunCommand
+)
+
+
+URL = "https://sourceforge.net/projects/boost/files/boost/1.61.0/boost_1_61_0.tar.gz"
+
+
+def InstallBoost(installPrefix):
+    if os.path.exists(installPrefix):
+        raise RuntimeError("{!r} installation already exists.".format(installPrefix))
+
+    PrintInfo("Current working directory: {}".format(os.getcwd()))
+
+    stagingDir = os.path.join(os.getcwd(), 'staging', 'boost')
+    MakeDirectories(stagingDir)
+
+    downloadDst = os.path.join(stagingDir, os.path.split(URL)[1])
+    if DownloadURL(URL, downloadDst):
+        Unpack(downloadDst, stagingDir)
+
+    buildDir = os.path.join(stagingDir, 'build')
+    MakeDirectories(buildDir)
+
+    srcDir = os.path.join(stagingDir, 'boost_1_61_0')
+    ChangeDirectory(srcDir)
+
+    bootstrapCmd = "./bootstrap.sh --prefix=\"{}\"".format(installPrefix)
+    RunCommand(bootstrapCmd)
+
+    b2Cmd = " ".join([
+        "./b2",
+        "--prefix={}".format(installPrefix),
+        "--build-dir={}".format(buildDir),
+        "-j8",
+        "address-model=64",
+        "link=shared",
+        "runtime-link=shared",
+        "threading=multi",
+        "-a",
+        "install"
+    ])
+    RunCommand(b2Cmd)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Installs boost.")
+    parser.add_argument('installPrefix', type=str, help="Directory where boost will be installed.")
+    args = parser.parse_args()
+    InstallBoost(args.installPrefix)
+
